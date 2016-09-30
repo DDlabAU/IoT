219dad8d7fff0bf1a0a819118364432cca4b9f8a
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2016-09-30 17:52:48 +0200

    Code that lights an LED when feed receives any data

---
+++ IFTTT_IO/subscribeLED/config.h
@@ -0,0 +1,10 @@
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+
+/******************************* WIFI **************************************/
+
+
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
