3bd7783e3fe96f47886df821bc2cfdd39e05e87d
NBAKH <hr.nbakh@gmail.com>
2018-02-19 12:36:23 +0100

    example rdy

--- MQTT/mqtt_p5js/sketch.js
+++ MQTT/mqtt_p5js/sketch.js
@@ -1,32 +1,33 @@
 // required when running on node.js
 // var mqtt = require('mqtt');
+
+var user = "javascripttest";
+var token = "0a5c2e61be56cb42";
+var client;
+
 function setup(){
-
-}
-
-function draw(){
-  background(255,0,0);
-  ellipse(56, 46, 55, 55);
-}
-
-function mouseClicked(){
-  var client = mqtt.connect('mqtt://try:try@broker.shiftr.io', {
+  client = mqtt.connect('mqtt://'+ user + ':' + token + '@broker.shiftr.io', {
     clientId: 'javascript'
   });
 
   client.on('connect', function(){
     console.log('client has connected!');
-
-    client.subscribe('/example');
-    // client.unsubscribe('/example');
-
-    setInterval(function(){
-      client.publish('/hello', 'katte');
-    }, 500);
   });
 
   client.on('message', function(topic, message) {
     console.log('new message:', topic, message.toString());
+    if(message.toString() == 'the mouse is out'){
+      client.publish('message/receiced', 'too late to set the trap');
+    }
   });
+  client.subscribe('/+/mouse');
+  // client.unsubscribe('/example');
+}
 
+function draw(){
+  background(255,0,0);
+}
+
+function mouseClicked(){
+  client.publish('/hello/mouse', 'the mouse is out');
 }
